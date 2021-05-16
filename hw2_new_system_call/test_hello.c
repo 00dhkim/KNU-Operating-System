5a12f86c607891002fa494cdd6fc5c63b83c0094
Dohyun Kim <00dh.kim@gmail.com>
2021-05-16 14:39:54 +0900

    read and write success

--- hw2_new_system_call/test_hello.c
+++ hw2_new_system_call/test_hello.c
@@ -17,6 +17,7 @@ int main() {
     for(int i=0;i<10;i++) {
         printf("[%d]%d %c\n",i,buf[i],buf[i]);
     }
+    printf("\n--------\n");
     // read(fdin, buf, 11);
     syscall(__NR_sread, fdin, buf, 11);
 
