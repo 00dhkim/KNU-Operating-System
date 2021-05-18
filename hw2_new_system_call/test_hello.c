0932044164ddc8f9b4e9ea06fa486a5484dedb76
Dohyun Kim <00dh.kim@gmail.com>
2021-05-18 16:34:10 +0900

    edit test_hello.c

--- hw2_new_system_call/test_hello.c
+++ hw2_new_system_call/test_hello.c
@@ -3,32 +3,14 @@
 #include <fcntl.h>
 
 #define __NR_hello 436
-#define __NR_swrite 437
-#define __NR_sread 438
+// #define __NR_swrite 437
+// #define __NR_sread 438
 
 int main() {
-    // int n=0;
-    // n = syscall(__NR_hello, 9, 7);
-    // printf("return value: %d\n", n);
 
-    char buf[1024];
-    int fdin = open("input.txt", O_RDONLY);
-
-    // read(fdin, buf, 11);
-    syscall(__NR_sread, fdin, buf, 7);
-    
-    for(int i=0;i<7;i++) {
-        printf("[%d]%d %c\n",i,buf[i],buf[i]);
-    }
-    printf("\n--------\n\n");
-
-    for(int i=0;i<7;i++) {
-        buf[i] = ~buf[i];
-    }
-
-    for(int i=0;i<7;i++) {
-        printf("[%d]%d %c\n",i,buf[i],buf[i]);
-    }
+    int n=0;
+    n = syscall(__NR_hello, 9, 7);
+    printf("return value: %d\n", n);
 
     return 0;
 }
